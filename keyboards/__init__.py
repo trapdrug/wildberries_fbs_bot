0dfd4b539a290aeaabb25d38e86a1ae892a67b2d
Yara3000 <eeeeeyara3000@gmail.com>
2026-07-19 02:46:06 +0300

    fix: исправлен ImportError в keyboards/__init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,7 +1,8 @@
 from .inline import (
     get_create_supply_keyboard,
     get_order_items_keyboard,
-    get_confirm_keyboard,
-    get_add_more_keyboard,
+    get_trbx_keyboard,
+    get_confirm_supply_keyboard,
+    get_supply_items_keyboard,
     get_main_menu_keyboard,
 )
